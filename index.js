b2eb7572e39ee288181c56fd9f17a6bb059689ad
Deepak Pai <paidpk07@gmail.com>
2019-06-30 22:21:02 +0530

    Port changes for deploying to Heroku

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var server_port = process.env.PORT || 8080;
 
 const bodyParser = require("body-parser");
 const routes = require("./routes");
